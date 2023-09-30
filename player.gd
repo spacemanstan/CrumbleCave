51c66b77ae8b0071941570dd912a7b8eb1e961e4
spacemanstan <ariston.bodnarchuk@gmail.com>
2023-09-30 16:31:13 -0600

    second stone type added

--- player.gd
+++ player.gd
@@ -20,7 +20,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump using custom "jump" action.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction.
