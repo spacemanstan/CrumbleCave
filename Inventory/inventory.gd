9e3c72dd80184796db6e238b80297b7f954e9f46
Kyllo Brooks <kierankyllo@users.noreply.github.com>
2023-10-01 15:48:07 -0600

    Update inventory.gd
    pasted from my local

--- Inventory/inventory.gd
+++ Inventory/inventory.gd
@@ -3,15 +3,16 @@ extends Control
 var inventory: Array[ItemData]
 var hbox: HBoxContainer
 var focused_index: int = -1  # The index of the focused item in the HBoxContainer
-
-var weight = 0
-var score = 15
+var total_weight = 0
+var score = 0
 
 func _ready():
 	hbox = $CenterContainer/HBoxContainer
 	populate_inventory()
+	calculate_weight_and_score()
 
 func _process(delta):
+	#TODO add inventory max quantity
 	if Input.is_action_just_pressed("ui_scroll_left"):
 		if focused_index > 0:  # if not the first item
 			focused_index -= 1
@@ -23,6 +24,17 @@ func _process(delta):
 	elif Input.is_action_just_pressed("discard"):
 		remove_item_from_inventory()
 
+# calculate the current total weight and score for the player
+func calculate_weight_and_score():
+	total_weight = 0
+	score = 0
+	for item_data in inventory:
+		total_weight += item_data.weight
+		score += item_data.weight * item_data.weight
+	print("Total Weight:", total_weight)
+	print("Score:", score)
+
+# populate the inventory
 func populate_inventory():
 	clear_inventory()
 	for item_data in inventory:
@@ -31,6 +43,7 @@ func populate_inventory():
 	if hbox.get_child_count() == 1:
 		focused_index = 0
 		focus_item_at_index(focused_index)
+	calculate_weight_and_score()
 
 func add_item_to_inventory(item_data: ItemData):
 	inventory.append(item_data)
@@ -49,6 +62,7 @@ func add_item_to_inventory(item_data: ItemData):
 	if hbox.get_child_count() == 1:
 		focused_index = 0
 		focus_item_at_index(focused_index)
+	calculate_weight_and_score()
 
 func focus_item_at_index(index: int):
 	for i in range(hbox.get_child_count()):
@@ -73,6 +87,7 @@ func remove_item_from_inventory():
 	else:
 		# If no items left, reset the focus
 		unfocus_all_items()
+	calculate_weight_and_score()
 
 func unfocus_all_items():
 	focused_index = -1
@@ -87,15 +102,28 @@ func clear_inventory():
 	for child in hbox.get_children():
 		hbox.remove_child(child)
 		child.queue_free()
+	calculate_weight_and_score()
 
 func _on_gems_red_gem_collected(body):
 	var item = ItemData.new()
-	item.name = body.name
+	item.name = body.gemName
+	item.weight = body.weight
 	var placeholder = PlaceholderTexture2D.new()
 	placeholder.set_size(Vector2(75,155))
-	item.texture = placeholder
+	# Handle the different textures and add them to the ItemData
+	# TODO: replace placeholder with proper textures for each Gem type
+	match item.name:
+		"Gem A":
+			item.texture = placeholder
+		"Gem B":
+			item.texture = placeholder
+		"Gem C":
+			item.texture = placeholder
+		"Gem D":
+			item.texture = placeholder
 	add_item_to_inventory(item)
 	print("PLAYER COLLECTED ", body.gemName)
+	calculate_weight_and_score()
 
 func _on_world_game_over():
 	clear_inventory()
